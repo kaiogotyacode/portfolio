27271fd6ec9efab9eb657f968eea557b930903e3
Kaio Maciel <43120150+kaiogotyacode@users.noreply.github.com>
2023-02-01 07:09:08 -0300

    Fixing files and Preparing future changes

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,39 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿/* ======================================= MENU JS ======================================= */
 
-// Write your JavaScript code.
+window.addEventListener("load", () => {
+
+    var menu = document.getElementById("menu");
+    var navOptions = document.getElementById("nav-options");
+    var handle = false;
+
+    menu.addEventListener("click", () => {
+        if (handle) {
+            navOptions.style.cssText += "display: none;";
+            menu.style.cssText += "background-color: transparent;color: #ffffff";
+            handle = false;
+        } else {
+            navOptions.style.cssText += "display: flex;";
+            menu.style.cssText += "background-color: #f1f2f6;color: #000";
+
+            handle = true;
+        }
+    });
+
+
+    //para MD >> toda vez que o Menu tiver o display modificado, forçar o display flex da NAV
+    window.addEventListener("resize", () => {
+        if (window.innerWidth > 767) {
+            navOptions.style.cssText += "display: flex;";
+            menu.style.cssText += "background-color: transparent;color: #ffffff";
+            handle = false;
+        }
+        if (window.innerWidth < 767) {
+            navOptions.style.cssText += "display: none;";
+            menu.style.cssText += "background-color: transparent;color: #ffffff";
+            handle = false;
+        }
+    })
+
+})
+
+/* ======================================= MENU JS ======================================= */
