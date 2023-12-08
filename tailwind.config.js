a6777e9c2675a6965c9a594ff6bb82675737ea16
Kaio Maciel <kaioreis1153@hotmail.com>
2023-12-08 08:41:55 -0300

    feat: Compressed Files

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,8 @@ module.exports = {
         divisor: 'divisor 3s ease-in-out infinite',
         switchLanguageAnimator: 'onSwitchLanguage 1s ease-in-out',
         snowFalling: 'snowFalling 15s ease-in-out infinite',
-        thunderOne: 'thunderOne 10s ease-in-out infinite',
-        thunderTwo: 'thunderTwo 10s ease-in-out infinite'
+        thunderOne: 'thunderOne 6s ease-in-out infinite',
+        thunderTwo: 'thunderTwo 9s ease-in-out infinite'
       }
     },
   },
